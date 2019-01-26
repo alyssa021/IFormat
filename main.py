f4e0f65f02d75abaac34fbe13749adf809bd06c8
Alyssa Nolan <canalyssa@outlook.com>
2019-01-26 22:30:04 +0530

    Success parsing html tags

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from __future__ import print_function
+import re;
 
 src_file = open("test/html/test1.html", "r");
 src_file_content = src_file.read();
+
+class HTML_Tag:
+  def __init__(self, raw_tag):
+    re_exp = re.match("<[/]?(.*?)>", raw_tag);
+    self.name = re_exp.group(1);
+
+    if("/" in raw_tag):
+      self.closing = True;
+    else:
+      self.closing = False;
 
 is_tag = False;
 tags = [];
@@ -19,4 +30,6 @@ for ch in src_file_content:
   if is_tag:
     tag += ch;
 
-print(tags);
+for tag in tags:
+  _tag = HTML_Tag(tag);
+  print(_tag.name);
