7641fd431617ab9b26bc1ff1b5032c6d08172196
Alyssa Nolan <canalyssa@outlook.com>
2019-01-26 22:17:14 +0530

    Parsed html tags

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from __future__ import print_function
-from html.parser import HTMLParser
 
-src_file = open("testfile.html", "r");
+src_file = open("test/html/test1.html", "r");
 src_file_content = src_file.read();
 
-parser = HTMLParser();
-print(parser.feed(src_file_content));
+is_tag = False;
+tags = [];
+for ch in src_file_content:
+  if ch == "<":
+    tag = "";
+    is_tag = True;
+
+  elif ch == ">":
+    tag += ch;
+    is_tag = False;
+
+    tags.append(tag);
+
+  if is_tag:
+    tag += ch;
+
+print(tags);
